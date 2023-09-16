f9ebadc7cefddc01eeaf9d455d392ba021debfa5
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-16 09:15:17 -0400

    this

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ siggy.sleep(5);
 console.log(didiWU, nico, siggy);
  */
 
-function Person(name, energy) {
+/* function Person(name, energy) {
   let person = Object.create(Person.prototype);
   person.name = name;
   person.energy = energy;
@@ -63,6 +63,36 @@ Person.prototype.sleep = function (length) {
 const nico = Person("nico", 8);
 const didiWU = Person("Didi Wu", 100);
 const siggy = Person("Siggy", 5);
+didiWU.eating(10);
+nico.studing(5);
+siggy.sleep(5);
+console.log(didiWU, nico, siggy); */
+
+function Person(name, energy) {
+  this.name = name;
+  this.energy = energy;
+}
+
+Person.prototype.lifting = function (length) {
+  console.log(`${this.name} is lifting`);
+  this.energy -= length;
+};
+Person.prototype.studing = function (length) {
+  console.log(`${this.name} is studing`);
+  this.energy -= length;
+};
+Person.prototype.eating = function (amount) {
+  console.log(`${this.name} is eating`);
+  this.energy += amount;
+};
+Person.prototype.sleep = function (length) {
+  console.log(`${this.name} is sleeping`);
+  this.energy += length;
+};
+
+const nico = new Person("nico", 8);
+const didiWU = new Person("Didi Wu", 100);
+const siggy = new Person("Siggy", 5);
 didiWU.eating(10);
 nico.studing(5);
 siggy.sleep(5);
