bbe5429ecbd8ebc1a437a322951b8bc4e6be2118
Nicolas Fuentes <nfuentes11@gmail.com>
2023-09-22 06:14:21 -0400

    __proto__.__proto__

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-// Initialize a constructor function for a new Hero
-function Hero(name, level) {
-  this.name = name;
-  this.level = level;
-}
-
-// Add greet method to the Hero prototype
-Hero.prototype.greet = function () {
-  return `${this.name} says hello.`;
+let animal = {
+  eat: true,
+  walk() {
+    return "Animal walk";
+  },
 };
 
-function Warrior(name, level, weapon) {
-  Hero.call(this, name, level);
-  this.weapon = weapon;
-}
-// Initialize Healer constructor
-function Healer(name, level, spell) {
-  Hero.call(this, name, level);
-
-  this.spell = spell;
-}
-
-Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
-Object.setPrototypeOf(Healer.prototype, Hero.prototype);
-
-// All other prototype methods added below
-Warrior.prototype.attack = function () {
-  return `${this.name} attacks with the ${this.weapon}.`;
+let rabbit = {
+  jumps: true,
+  __proto__: animal,
 };
 
-Healer.prototype.heal = function () {
-  return `${this.name} casts ${this.spell}.`;
+rabbit.walk = function () {
+  return "Rabbit! Bounce-bounce!";
 };
-
-const hero1 = new Hero("Didi Wu TinTin", 1);
-const hero2 = new Warrior("Nico", 1, "axe");
-const hero3 = new Healer("Siggy", 1, "Black Magick");
-
-console.log(hero1);
-console.log(Object.getPrototypeOf(hero1)); //Hero
-console.log(hero1.greet());
-console.log(hero2.attack());
-console.log(hero2.greet());
+let longEar = {
+  earLength: 10,
+  __proto__: animal,
+};
+console.log(rabbit.eat);
+console.log(rabbit.walk());
+console.log(longEar.walk());
