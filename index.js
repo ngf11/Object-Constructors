8ebb8868d83ab01bf9ef31afb66f16c5177f2cb5
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-16 11:59:53 -0400

    Prototypal inheritance

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ Book.prototype.info = function () {
 const beHereNow = new Book("Be Here Now", "Ram Dass", 369, "In Progress");
 console.log(beHereNow);
 console.log(beHereNow.info());
+console.log(Object.getPrototypeOf(beHereNow) === Book.prototype);
+//---Prototypal inheritance----
+console.log(Object.getPrototypeOf(Book.prototype) === Object.prototype);
+console.log(beHereNow.valueOf());
+console.log(Book.hasOwnProperty("valueOf"));
+console.log(Object.prototype.hasOwnProperty("valueOf"));
+console.log(Object.prototype.hasOwnProperty("hasOwnProperty"));
