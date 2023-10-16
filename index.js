73c1c44ea87ab53ec8379c27ce09bfd1a4c3085a
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-16 11:12:59 -0400

    Object Constractorts

--- index.js
+++ index.js
@@ -1,139 +1,18 @@
-/* let animal = {};
-animal.name = "Milo";
-animal.energy = 10;
-
-animal.eat = function (amount) {
-  console.log(`${this.name} is eating `);
-  this.energy += amount;
-};
-
-amimal.sleep = function (length) {
-  console.log(`${this.name} is sleeping`);
-  this.energy += length;
-};
-
-amimal.play = function (length) {
-  console.log(`${this.name} is playing`);
-  this.energy -= length;
-};
-let animalMethods = {
-    eat: function (amount) {
-        console.log(`${this.name} is eating `);
-        this.energy += amount;
-    },
-    
-    sleep: function (length) {
-        console.log(`${this.name} is sleeping`);
-        this.energy += length;
-    },
-    
-    play: function (length) {
-        console.log(`${this.name} is playing`);
-        this.energy -= length;
-    },
-};
-*/
-
-/* function Animal(name, energy) {
-  let animal = Object.create(Animal.prototype);
-  animal.name = name;
-  animal.energy = energy;
-
-  return animal;
-}
-Animal.prototype.eat = function (amount) {
-  console.log(`${this.name} is eating `);
-  this.energy += amount;
-};
-
-Animal.prototype.sleep = function (length) {
-  console.log(`${this.name} is sleeping`);
-  this.energy += length;
-};
-
-Animal.prototype.play = function (length) {
-  console.log(`${this.name} is playing`);
-  this.energy -= length;
-}; */
-
-// function Animal(name, energy) {
-//   this.name = name;
-//   this.energy = energy;
-// }
-// Animal.prototype.eat = function (amount) {
-//   console.log(`${this.name} is eating `);
-//   this.energy += amount;
-// };
-
-// Animal.prototype.sleep = function (length) {
-//   console.log(`${this.name} is sleeping`);
-//   this.energy += length;
-// };
-
-// Animal.prototype.play = function (length) {
-//   console.log(`${this.name} is playing`);
-//   this.energy -= length;
-// };
-/* 
-class Animal {
-  constructor(name, energy) {
-    this.name = name;
-    this.energy = energy;
-  }
-  eat(amount) {
-    console.log(`${this.name} is eating `);
-    this.energy += amount;
-  }
-  sleep(length) {
-    console.log(`${this.name} is sleeping`);
-    this.energy += length;
-  }
-  play(length) {
-    console.log(`${this.name} is playing`);
-    this.energy -= length;
-  }
-}
-const milo = new Animal("Milo", 5);
-const cami = new Animal("Camilia", 3);
-cami.sleep(3);
-milo.play(0.5);
-console.log(milo);
-console.log(cami);
-
-for (let key in milo) {
-  if (milo.hasOwnProperty(key)) {
-    console.log(key + ": " + milo[key]);
-  }
-}
- */
-
-//
-class Animal {
-  constructor(name, energy) {
-    this.name = name;
-    this.energy = energy;
-  }
-  eat(amount) {
-    console.log(`${this.name} is eating `);
-    this.energy += amount;
-  }
-  sleep(length) {
-    console.log(`${this.name} is sleeping`);
-    this.energy += length;
-  }
-  play(length) {
-    console.log(`${this.name} is playing`);
-    this.energy -= length;
-  }
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.SayName = function () {
+    console.log(`Hi my name is ${this.name}`);
+  };
 }
 
-class Dog extends Animal {
-  constructor(name, energy, breed) {
-    super(name, energy);
-    this.breed = breed;
-  }
-  bark() {
-    console.log("woof Woof");
-    this.energy -= 0.5;
-  }
-}
+const nico = new Player("nico", "X");
+const didi = new Player("Didi wu TinTin", "O");
+console.log(nico);
+console.log(didi);
+console.log(nico.name);
+console.log(didi.name);
+console.log(nico.marker);
+console.log(didi.marker);
+nico.SayName();
+didi.SayName();
