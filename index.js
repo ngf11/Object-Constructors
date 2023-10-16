4d5be4dac0558373f229e7100bda7ffc35551761
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-16 13:18:05 -0400

    inheritance

--- index.js
+++
@@ -1,2 +0,0 @@
-//---Prototypal inheritance----
-let x = {};
--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+//---Prototypal inheritance----
+let x = {};
+console.log(Object.getPrototypeOf(x));
+console.log(x.__proto__);
 
+let y = [];
+console.log(y.__proto__);
