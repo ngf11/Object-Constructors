f9d71230d4ea5dcbbd1f28efac6c88be1797ce95
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-16 13:51:16 -0400

    [prototype]

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 //---Prototypal inheritance----
-let x = {};
-console.log(Object.getPrototypeOf(x));
-console.log(x.__proto__);
+function Hero(name, level) {
+  this.name = name;
+  this.level = level;
+}
 
-let y = [];
-console.log(y.__proto__);
+Hero.prototype.greet = function () {
+  return `${this.name} say Hello`;
+};
+
+function Warrior(name, level, weapon) {
+  Hero.call(this, name, level);
+  this.wepon = weapon;
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+
+  this.spell = spell;
+}
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attacks with the ${this.weapon}.`;
+};
+
+Healer.prototype.heal = function () {
+  return `${this.name} casts ${this.spell}.`;
+};
+
+const didi = new Hero("Didi Wu", 100);
+console.log(didi.greet());
+
+const nico = new Warrior("nico", 10, "axe");
+console.log(nico);
+console.log(nico.greet());
+
+const siggy = new Healer("siggy", 10, "Black magic");
+console.log(siggy);
+console.log(siggy.greet());
